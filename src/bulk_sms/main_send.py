acbbac64fa6848bd4d904560ca81069de3f55b6a
Ryan Echols <rkechols21@gmail.com>
2026-09-08 20:20:12 -0600

    reset project; build with uv

---
+++ src/bulk_sms/main_send.py
@@ -0,0 +1,6 @@
+def main():
+    print("TODO: send requests to the phone over local network")
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/bulk_sms/main_server.py
@@ -0,0 +1,6 @@
+def run_server():
+    print("TODO: run the phone-side server!")
+
+
+if __name__ == "__main__":
+    run_server()
