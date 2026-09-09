4d5b00dc8eebf126b8b9badbaca10184490ca2bd
Ryan Echols <rkechols21@gmail.com>
2026-09-08 21:05:27 -0600

    set up checks

--- src/bulk_sms/main_server.py
+++ src/bulk_sms/main_server.py
@@ -1,5 +1,18 @@
+from argparse import ArgumentParser
+
+import uvicorn
+from fastapi import FastAPI
+
+api = FastAPI(title="Bulk SMS Server")
+
+
 def main():
-    print("TODO: run the phone-side server!")
+    arg_parser = ArgumentParser(description="Run phone-side server for bulk SMS operations")
+    arg_parser.add_argument("--port", "-p", type=int, default=8080)
+    args = arg_parser.parse_args()
+    port: int = args.port
+
+    uvicorn.run(api, host="0.0.0.0", port=port)
 
 
 if __name__ == "__main__":
---
+++ src/bulk_sms/schemas.py
@@ -0,0 +1,19 @@
+import re
+from typing import Annotated
+
+from pydantic import AfterValidator, BaseModel
+
+
+def _validate_usa_phone_number(s: str) -> str:
+    s = re.sub(r"-\s\(\)", "", s)  # Strip hyphens, whitespace, and parens
+    if not re.fullmatch(r"\+1\d{10}", s):
+        raise ValueError("USA phone number must start with +1 and then contain exactly 10 additional digits")
+    return s
+
+
+type USAPhoneNumber = Annotated[str, AfterValidator(_validate_usa_phone_number)]
+
+
+class Recipients(BaseModel):
+    copy_on_all: dict[str, USAPhoneNumber]
+    groups: dict[str, list[USAPhoneNumber]]
