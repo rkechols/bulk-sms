96c2f980ed718b3cc5595cf73f6e161b951633da
Ryan Echols <rkechols21@gmail.com>
2026-09-08 21:21:12 -0600

    generate random password

--- src/bulk_sms/main_server.py
+++ src/bulk_sms/main_server.py
@@ -1,9 +1,33 @@
+import random
+import string
 from argparse import ArgumentParser
+from contextlib import asynccontextmanager
+from typing import Annotated
 
 import uvicorn
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, Request
 
-api = FastAPI(title="Bulk SMS Server")
+
+@asynccontextmanager
+async def lifespan(app_: FastAPI):
+    password = "".join(random.choices(string.digits + string.ascii_uppercase[:6], k=8))
+    app_.state.password = password
+    print(f"PASSWORD: {password}")  # noqa: T201
+    yield
+
+
+def _get_password(request: Request) -> str:
+    return request.app.state.password
+
+
+type PasswordDep = Annotated[str, Depends(_get_password)]
+
+app = FastAPI(title="Bulk SMS Server", lifespan=lifespan)
+
+
+# @app.get("/")
+# async def get_root(password: PasswordDep) -> HTMLResponse:
+#     return HTMLResponse(content=f"<pre>Password: {password}</pre>")
 
 
 def main():
@@ -12,7 +36,7 @@ def main():
     args = arg_parser.parse_args()
     port: int = args.port
 
-    uvicorn.run(api, host="0.0.0.0", port=port)
+    uvicorn.run(app, host="0.0.0.0", port=port)
 
 
 if __name__ == "__main__":
